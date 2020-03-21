6d56ec1153335638ca7e240f7912fc92b062e56f
mugsyb123 <grantmugsy@gmail.com>
2020-03-21 00:52:15 -0400

    Add project files.

---
+++ iRecipt/iRecipt.Android/PhotoPickerService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using System.IO;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using iRecipt.Droid;
+
+[assembly: Dependency(typeof(PhotoPickerService))]
+namespace iRecipt.Droid
+{
+    class PhotoPickerService : ImageUpload
+    {
+        public Task<Stream> GetImageStreamAsync()
+        {
+            // Define the Intent for getting images
+            Intent intent = new Intent();
+            intent.SetType("image/*");
+            intent.SetAction(Intent.ActionGetContent);
+
+            // Start the picture-picker activity (resumes in MainActivity.cs)
+            MainActivity.Instance.StartActivityForResult(
+                Intent.CreateChooser(intent, "Select Picture"),
+                MainActivity.PickImageId);
+
+            // Save the TaskCompletionSource object as a MainActivity property
+            MainActivity.Instance.PickImageTaskCompletionSource = new TaskCompletionSource<Stream>();
+
+            // Return Task object
+            return MainActivity.Instance.PickImageTaskCompletionSource.Task;
+        }
+    }
+}
---
+++ iRecipt/iRecipt/ReciptPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace iRecipt
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ReciptPage : ContentPage
+    {
+        Image image;
+
+        public ReciptPage()
+        {
+            InitializeComponent();
+
+            ChooseRecipt.Clicked += OnPickPhotoButtonClicked;
+
+            
+        }
+
+        async void OnPickPhotoButtonClicked(object sender, EventArgs e)
+        {
+            (sender as Button).IsEnabled = false;
+
+            Stream stream = await DependencyService.Get<ImageUpload>().GetImageStreamAsync();
+            if (stream != null)
+            {
+                image.Source = ImageSource.FromStream(() => stream);
+            }
+
+                (sender as Button).IsEnabled = true;
+        }
+    }
+}
